cc5b880ea9adf0bd7a5e568d3a47f217ccd0d21a
jyh7a <jyh7a@naver.com>
2020-01-21 16:05:51 +0900

    햄버그 버튼 750px이상일때 스크립트수정

--- Blooger/js/main.js
+++ Blooger/js/main.js
@@ -1,12 +1,16 @@
-$(document).ready(function(){
-
-  $nav = $('.nav');
-  $toggleCollapse = $('.toggle-collapse')
+$(document).ready(function() {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
 
   // click evnet on toggle menu
-  $toggleCollapse.click(function(){
-    $nav.toggleClass('collapse');
+  $toggleCollapse.click(function() {
+    $nav.toggleClass("collapse");
+  });
 
-  })
-
+  // delete collapse class if width more than 750px
+  $(window).resize(function() {
+    if (window.innerWidth > 750) {
+      $nav.removeClass("collapse");
+    }
+  });
 });
